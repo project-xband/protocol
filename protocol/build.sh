99e84fa12268e4b88807c5c470b2b059f2620f0d
Dennis Aleynikov <daleynikov@gmail.com>
2016-04-26 14:53:32 -0700

    make builds use vars

--- protocol/build.sh
+++ protocol/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # em++ -O2 common.hpp -o common.bc
-em++ -O2 interface.cpp -o interface.bc
-em++ -O2 protocol.cpp -o protocol.bc
-em++ -O2 device.cpp -o device.bc
-em++ -O2 test.cpp -o test.bc
-em++ -O2 protocol.bc device.bc interface.bc test.bc -o project.bc
-em++ -O2 project.bc -o project.html
+CC=emcc
+OPT=
+$CC $OPT interface.cpp -o interface.bc
+$CC $OPT protocol.cpp -o protocol.bc
+$CC $OPT device.cpp -o device.bc
+$CC $OPT test.cpp -o test.bc
+$CC $OPT --bind protocol.bc device.bc interface.bc test.bc -o c.bc
+$CC $OPT c.bc -o c.js
--- protocol/clean.sh
+++ protocol/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # rm -rf protocol.bc device.bc interface.bc test.bc project.bc
-rm -rf ./*.js.mem ./*.js ./*.bc ./*.html.mem ./*.html ./*.data
+rm -rf ./*.js.mem ./project.js ./*.bc ./*.html.mem ./*.html ./*.data
